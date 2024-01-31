2523e0e19f3dce4524a4e7b8b16f5f02f6942abf
ctchen42 <ctchen42@student.42.fr>
2024-01-31 11:01:53 +0100

    ajout de irc_op a user + fonctions

--- obj/server.d
+++
@@ -1,8 +0,0 @@
-obj/server.o: src/server.cpp include/irc.hpp include/color.h \
- include/User.hpp include/irc.hpp include/Channel.hpp include/User.hpp
-include/irc.hpp:
-include/color.h:
-include/User.hpp:
-include/irc.hpp:
-include/Channel.hpp:
-include/User.hpp:
