b1c6f85a58b6a0a019a62d6f1ebbd8be63badd35
alselnet <alex.selnet@gmail.com>
2024-02-02 17:20:14 +0100

    connexion established with the client

--- obj/channel.d
+++
@@ -1,9 +0,0 @@
-obj/channel.o: src/channel.cpp src/../include/irc.hpp \
- src/../include/color.h src/../include/User.hpp src/../include/irc.hpp \
- src/../include/Channel.hpp src/../include/Channel.hpp
-src/../include/irc.hpp:
-src/../include/color.h:
-src/../include/User.hpp:
-src/../include/irc.hpp:
-src/../include/Channel.hpp:
-src/../include/Channel.hpp:
