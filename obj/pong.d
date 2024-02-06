e2df55c7091a42579cbd9bdd7dbc0b45a4d38cd0
Jules <julesthuys@gmail.com>
2024-02-06 15:25:39 +0100

    w/o .o

--- obj/pong.d
+++
@@ -1,8 +0,0 @@
-obj/pong.o: src/pong.cpp src/../include/irc.hpp src/../include/color.h \
- src/../include/User.hpp src/../include/irc.hpp \
- src/../include/Channel.hpp
-src/../include/irc.hpp:
-src/../include/color.h:
-src/../include/User.hpp:
-src/../include/irc.hpp:
-src/../include/Channel.hpp:
--- obj/quit.d
+++
@@ -1,8 +0,0 @@
-obj/quit.o: src/quit.cpp src/../include/irc.hpp src/../include/color.h \
- src/../include/User.hpp src/../include/irc.hpp \
- src/../include/Channel.hpp
-src/../include/irc.hpp:
-src/../include/color.h:
-src/../include/User.hpp:
-src/../include/irc.hpp:
-src/../include/Channel.hpp:
