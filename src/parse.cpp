1554dc8f74479c031af8382b4b5ad8933c5be4a5
ctchen42 <ctchen42@student.42.fr>
2024-02-06 13:11:58 +0100

    exec_command + channel.cpp changes

--- src/parse.cpp
+++ src/parse.cpp
@@ -6,7 +6,7 @@
 /*   By: aselnet <aselnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:22:50 by jthuysba          #+#    #+#             */
-/*   Updated: 2024/02/06 12:43:59 by aselnet          ###   ########.fr       */
+//   Updated: 2024/02/06 13:11:21 by ctchen           ###   ########.fr       //
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,25 @@ std::list<User>::iterator getUser( int clientSockFd, irc * irc_data )
 	return (ite);
 }
 
+std::list<Channel>::iterator getChannel( std::string username, irc * irc_data )
+{
+	std::list<Channel>::iterator	it = irc_data->channelList.begin();
+	std::list<Channel>::iterator	ite = irc_data->channelList.end();
+
+	for (; it != ite; it++)
+	{
+		if (it->findUserI(username, irc_data->usersList) != irc_data->usersList.end())
+			return (it);
+	}
+	return (ite);
+}
+
 // Execute la commande dans str
 void	execute_command( std::string str, int clientSockFd, irc * irc_data )
 {
 	std::string	cmd;
 	std::istringstream	iss(str);
+	std::string	username = getUser(clientSockFd, irc_data)->getUsername();
 	
 	iss >> cmd;
 
@@ -57,24 +71,26 @@ void	execute_command( std::string str, int clientSockFd, irc * irc_data )
 	{
 		pong(clientSockFd);
 	}
-	else if (!cmd.compare("MODE"))
+	else if (cmd == "MODE")
 	{
 		std::string mode_reply = ":" + SERVER_NAME + " MODE abc +i\r\n";
+		getChannel(username, irc_data)->findUserinCh(username);
 		send(clientSockFd, mode_reply.c_str(), mode_reply.size(), 0);
 	}
-//	else if (cmd == "TOPIC")
-//	{
-//		getChannel()->changeTopic(getUser(clientSockFd, irc_data), str);
-//	}
+	else if (cmd == "TOPIC")
+	{
+		getChannel(username, irc_data)->changeTopic(
+			getChannel(username, irc_data)->findUserinCh(username), str);
+	}
 //	else if (cmd == "KICK")
-//	{Readaptation en cours
-//		getChannel()->kickUser(getUser(clientSockFd, irc_data), target);
+//	{//Readaptation en cours
+//		getChannel(username, irc_data)->kickUser(getUser(clientSockFd, irc_data), target);
 //	}
 //	else if (cmd == "INVITE")
 //	{Readaptation en cours
 //		getChannel()->addUser(getUser(clientSockFd, irc_data), target);
 //	}
-	// WIP => Toutes les autres commandes a ajouter
+	// WIP => Toutes les autres commandes a ajouterelse if (!cmd.compare("MODE"))
 }
 
 // parse la transmission ligne par ligne et execute chaque commande
