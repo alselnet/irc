ee63739d3a90a638b371fedcc7e6f0e1b9ae76ae
Jules <julesthuys@gmail.com>
2024-02-16 19:02:47 +0100

    ERR_NOSUCHNICK

--- src/private_msg.cpp
+++ src/private_msg.cpp
@@ -6,12 +6,13 @@
 /*   By: jthuysba <jthuysba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 17:29:46 by jthuysba          #+#    #+#             */
-//   Updated: 2024/02/16 14:59:25 by ctchen           ###   ########.fr       //
+/*   Updated: 2024/02/16 18:53:28 by jthuysba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../include/irc.hpp"
-# include "../include/Notif.hpp" 
+# include "../include/Notif.hpp"
+# include "../include/Error.hpp"
 
 void	private_msg(std::string str, int clientSockFd, irc *irc_data)
 {
@@ -41,6 +42,13 @@ void	private_msg(std::string str, int clientSockFd, irc *irc_data)
 	{
 		std::list<User>::iterator	origin_user = get_user(clientSockFd, irc_data);
 		std::list<User>::iterator	targetUser = get_user_by_nick(target, irc_data);
+		
+		if (targetUser == irc_data->usersList.end()) // Si le nick n'existe pas
+		{
+			Error	ERR_NOSUCHNICK(401, target, "", "No such nick/channel");
+			ERR_NOSUCHNICK.to_client(clientSockFd);
+			return ;
+		}
 		
 		std::string	id_string = origin_user->getNickname() + "!" + origin_user->getUsername() + "@" + origin_user->getIp(); // WIP => Username and hostname to get
 		Notif			message_to_send(id_string, "PRIVMSG", target, text);
